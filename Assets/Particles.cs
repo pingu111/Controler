4a298cb84f7d6075b81d404dc78b911250f1d74a
Paul <paul.turpin@telecom-sudgrenoble.eu>
2016-10-07 14:40:55 +0200

    particles

---
+++ Assets/Particles.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Particles : MonoBehaviour {
+
+
+    public bool isInContactWithWall;
+    private ParticleSystem.EmissionModule emission;
+	// Use this for initialization
+	void Start () {
+        emission = this.gameObject.GetComponent<ParticleSystem>().emission;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if(isInContactWithWall)
+        {
+            emission.enabled = true;
+        }
+        else
+        {
+            emission.enabled = false;
+
+        }
+    }
+}
